65b84ce5518e8da694fc0c12d442082dd61ac8e3
F <fahad.assoumani@epitech.eu>
2020-12-28 20:25:27 +0100

    [UPDATE] append query system and result drawer and their respectiv events events

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,13 @@
 #[path = "bitsline.rs"]
 mod bl;
+mod query;
+mod result;
 
-use std::cmp::{min, max};
 use tuikit::prelude::*;
+use tuikit::key::Key;
 
-fn main() {
-    let term: Term<()> = Term::with_options(TermOptions::default()
-                                                .height(TermHeight::Percent(30))
-                                                .mouse_enabled(true))
-                            .unwrap();
-    let mut row = 1;
-    let mut col = 0;
-
-    let _ = term.present();
-    let mut v_lines: [bl::BitsLine; 8] = [
+fn init_bit_table() -> [bl::BitsLine; 8] {
+    let v_lines: [bl::BitsLine; 8] = [
         bl::BitsLine::new(1),
         bl::BitsLine::new(2),
         bl::BitsLine::new(4),
@@ -23,26 +17,42 @@ fn main() {
         bl::BitsLine::new(64),
         bl::BitsLine::new(128)
     ];
+    v_lines
+}
+
+fn main() {
+    let term: Term<()> = Term::with_options(TermOptions::default()
+                                                .height(TermHeight::Percent(30))
+                                                .mouse_enabled(true))
+                            .unwrap();
+    let _ = term.present();
+    let mut query = query::Query::new();
+    let mut result = result::Results::new();
+    let mut v_lines = init_bit_table();
 
     while let Ok(ev) = term.poll_event() {
         let _ = term.clear();
 
-        let (width, height) = term.term_size().unwrap();
         match ev {
-            Event::Key(Key::ESC) | Event::Key(Key::Char('q')) => break,
-            Event::Key(Key::Up) => row = max(row-1, 1),
-            Event::Key(Key::Down) => row = min(row+1, height-1),
-            Event::Key(Key::Left) => col = max(col, 1)-1,
-            Event::Key(Key::Right) => col = min(col+1, width-1),
+            Event::Key(Key::ESC) | Event::Key(Key::Ctrl('c')) => break,
             Event::Key(Key::SingleClick(MouseButton::Left, _col, _row)) => {
                 check_bin_cells(&mut v_lines, Rectangle {top: _col as usize, left: _row as usize, width: 1, height: 1 });
-            }
+            },
+            Event::Key(Key::Char(ch)) => {
+                query.add_char_to_input(ch);
+            },
+            Event::Key(Key::Backspace) => query.rm_char_to_input(),
+            Event::Key(Key::Enter) => {
+                let res = query.get_input();
+                result.add_to_history(res);
+            },
             _ => {}
         }
+        term.draw(&query).unwrap();
+        term.draw(&result).unwrap();
         v_lines
             .iter()
             .for_each(|line| term.draw(line).unwrap());
-        let _ = term.set_cursor(row, col);
         let _ = term.present();
     }
 }
