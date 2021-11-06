bfafbd0d91324c30f9e9a917cc58a0bb363a73c8
F <fahad.assoumani@epitech.eu>
2021-11-06 05:05:37 +0100

    [UPDATE] result: adding cache vector and fews methods to format result output

--- src/result.rs
+++ src/result.rs
@@ -1,14 +1,22 @@
 use tuikit::prelude::*;
 
-// Dummy struct until operaions gets implemented
+#[path = "formatter.rs"]
+mod fm;
+
+type ResultLine = (char, isize);
+
+#[allow(dead_code)]
 pub struct Results {
     history: Vec<String>,
     pub res: Vec<String>,
+    pub tmp_buff: Vec<String>,
+    pub cache: Vec<ResultLine>,
 }
 
+#[allow(dead_code)]
 impl Results {
     pub fn new() -> Self {
-        Results { history: Vec::new(), res: Vec::new() }
+        Results { history: Vec::new(), res: Vec::new(), tmp_buff: Vec::new(), cache: Vec::new()}
     }
 
     fn is_only_space(&self, result: String) -> bool {
@@ -26,6 +34,10 @@ impl Results {
         }
     }
 
+    pub fn push_back_res(&mut self, result: String) {
+        self.res.push(result);
+    }
+
     pub fn push_front_res(&mut self, result: String) {
         if !result.is_empty() && !self.is_only_space(result.clone()) {
             self.res.insert(0, result);
@@ -33,10 +45,54 @@ impl Results {
             self.res.push(result);
         }
     }
+
+    pub fn transfert(&mut self) {
+        for elem in self.tmp_buff.iter() {
+            self.res.push(elem.to_string());
+        }
+        self.tmp_buff.clear();
+    }
+
+    pub fn add_to_cache(&mut self, res_line: ResultLine) {
+        self.cache.push(res_line);
+    }
+
+    fn dump_line_of_len(&mut self, length: usize) {
+        let line = (0..length).map(|_| "─").collect::<String>();
+        self.push_front_res(line);
+    }
+
+    pub fn format_result_cache(&mut self) {
+        if self.cache.len() == 0 {
+            return;
+        }
+        let iterator = self.cache.iter();
+        let buffer = iterator.map(|&x| { x.1 }).collect::<Vec<isize>>();
+
+        println!("{:?}", buffer[0].to_owned());
+        let maxes = fm::get_max_and_index(buffer);
+        let mut tmp: String;
+        let mut c: char;
+        let length = maxes.0.to_string().len();
+        let power = fm::get_power_of_two(maxes.0);
+
+        for elem in self.cache.clone() {
+            tmp = fm::align_values((elem.0, elem.1), power, length);
+            c = tmp.chars().nth(0).unwrap();
+            if (c != '=' && c != ' ') || c == '~' {
+               self.dump_line_of_len(tmp.len());
+            }
+            self.push_front_res(tmp)
+        }
+        self.push_front_res("#".to_string());
+        self.cache.clear();
+
+        println!("max {} ---------------------------- index {}", maxes.0, maxes.1);
+    }
 }
 
 impl Draw for Results {
-    fn draw(&self, canvas: &mut dyn Canvas) -> Result<()> {
+    fn draw(&self, canvas: &mut dyn Canvas) -> DrawResult<()> {
         let mut row: usize = 1;
         let attr = Attr { fg: Color::LIGHT_WHITE, ..Attr::default() };
         let len = self.res.len();
